a479825048e4891c6d2258b296a47de3d1639d69
NithinU2802 <nithinu2810@gmail.com>
2024-08-10 15:50:00 +0530

    CURD - Update

--- src/main/java/com/example/Web/service/ClubService.java
+++ src/main/java/com/example/Web/service/ClubService.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface ClubService {
     List<ClubDto> findAllClub();
     Club saveClub(Club club);
+
+    ClubDto findClubById(long clubId);
+
+    void updateClub(ClubDto clubDto);
 }
